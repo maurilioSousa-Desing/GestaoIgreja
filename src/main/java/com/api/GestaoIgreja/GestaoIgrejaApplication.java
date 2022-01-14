9a17db1f86b971f4401f879803a440290f329c33
maurilioSousa-Desing <fmaurilio6@gmail.com>
2022-01-14 19:46:49 -0300

    Create GestaoIgrejaApplication.java

---
+++ src/main/java/com/api/GestaoIgreja/GestaoIgrejaApplication.java
@@ -0,0 +1,13 @@
+package com.api.GestaoIgreja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoIgrejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoIgrejaApplication.class, args);
+	}
+
+}
