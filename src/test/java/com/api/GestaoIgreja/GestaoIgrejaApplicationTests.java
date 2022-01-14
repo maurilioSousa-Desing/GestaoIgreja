ce68d1627449ae15faf84775866c7a861034fce4
maurilioSousa-Desing <fmaurilio6@gmail.com>
2022-01-14 19:46:27 -0300

    Create GestaoIgrejaApplicationTests.java

---
+++ src/test/java/com/api/GestaoIgreja/GestaoIgrejaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.GestaoIgreja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoIgrejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
